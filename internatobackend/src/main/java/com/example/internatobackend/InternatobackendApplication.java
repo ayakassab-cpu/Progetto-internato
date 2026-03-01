890d7aedfa6b6ed8edf3c0d2e48fe4e1b39dba3d
ayakassab-cpu <aya.kassab@edu.unife.it>
2026-03-01 03:21:30 +0100

    initial commit

---
+++ internatobackend/src/main/java/com/example/internatobackend/InternatobackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.internatobackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternatobackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternatobackendApplication.class, args);
+	}
+
+}
---
+++ internatobackend/src/test/java/com/example/internatobackend/InternatobackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.internatobackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InternatobackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
